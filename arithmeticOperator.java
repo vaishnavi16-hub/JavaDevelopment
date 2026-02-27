9ae0120784a2523f36f54a2a82d7d11f2e520c88
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-02-27 11:58:00 +0530

    Add files via upload

---
+++ arithmeticOperator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class arithmeticOperator {
+	public static void main(String[] args) {
+		
+		 int a = 5;
+		 int b  = 2;
+		 int c = a+b;
+         
+		 System.out.println("Addition of" + a + "+" + b + "=" + c);
+		 System.out.println("Subtraction of" + a +"-" + b + "=" + (a-b));
+		 System.out.println("Multiplication of " + a +"*" + b + "=" +(a*b));
+		 System.out.println("Division of " + a +"/" + b + "=" +(a*b));
+		 
+		 
+		 
+	}
+
+}
---
+++ operatorDay2.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class operatorDay2 {
+  
+	public static void main(String[] args) {
+		int a= 40;
+		int b= 20;
+		int c= a+20;
+		System.out.println("Addition of " + a + " + " + b + " = " + c); 
+	    System.out.println("Sub of " + a + " - " + b + " = " + (a - b)); 
+		System.out.println("Mul of " + a + " * " + b + " = " + (a * b));
+		System.out.println("Div of " + a + " / " + b + " = " + (c / c) + " Quotient"); 
+		System.out.println("Mod of " + a + " % " + b + " = " + (c % b) + " Remainder"); 	}
+	 
+}
