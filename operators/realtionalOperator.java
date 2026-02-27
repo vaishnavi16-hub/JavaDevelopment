bd8cbd364468ad31255f15f9e0abbe69cd241c94
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-02-27 13:36:53 +0530

    Add files via upload

---
+++ operators/realtionalOperator.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class realtionalOperator {
+	
+	public static void main (String[] args) {
+		
+		int a= 10;
+		int b= 10;
+//		System.out.println(a>b);
+//		System.out.println(a>=b);
+//		System.out.println(a==b);// true
+		System.out.println(a!=b);
+	}
+  }
+
