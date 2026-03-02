8a3faca1a747547977e6d92fa675471cdf544052
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-02 13:28:04 +0530

    Add files via upload

---
+++ operators/notOperator.java
@@ -0,0 +1,37 @@
+package operators;
+
+public class notOperator {
+	
+// 1 example....
+//	public static void main(String[]args) {
+//		boolean isLogin = true;
+//		System.out.println(!isLogin);
+//	
+
+//	2example...
+//	public static void main(String[]args) {
+//		System.out.println(!(false)); // true
+//		
+//	}
+	
+//	3 example...
+//	public static void main (String[]args) {
+//		System.out.println(!(true));
+//	}  // false
+	    
+	
+//	4..
+//	public static void main (String[]args) {
+//		System.out.println(!(1>4));
+//	}  // 
+	
+	
+//	5...
+//	public static void main(String[]args) {
+//		System.out.println(!(1>4 && 10 < 8));
+//	} // true.
+	
+}
+
+
+
