76286d8d944cad3e38c2b898100748583eaf9620
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-06 23:29:22 +0530

    Add files via upload

---
+++ Control statements/src/Day1.java
@@ -0,0 +1,32 @@
+
+public class Day1 {
+	public static void main(String[]args) {
+//		int a = 56;
+//		if(a>10) {
+//			System.out.println("welcome A is greater than B ");
+//		}
+		
+		
+//		int a = 18;
+//		if(a>1) {
+//			System.out.println("This is eligible for voting");
+//		}
+		
+//		int a= 23;
+//		int b= 34;
+//		if(a<b) {
+//			System.out.println("Welcome..!");
+//		}
+		
+		 int num = 32;
+		 if (num % 2 == 0) {
+			 System.out.println("Even No - "+num);
+		 }else {
+			 System.out.println("Odd No =+ num");
+		 }
+		
+		
+		
+	}
+
+}
---
+++ Control statements/src/Even_Odd.java
@@ -0,0 +1,9 @@
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
