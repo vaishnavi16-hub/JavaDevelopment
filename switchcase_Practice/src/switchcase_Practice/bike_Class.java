f02be36facd97e07079224bb10a370dec59070f9
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-16 13:38:53 +0530

    Add files via upload

---
+++ switchcase_Practice/src/switchcase_Practice/bike_Class.java
@@ -0,0 +1,21 @@
+package switchcase_Practice;
+
+public class bike_Class {
+	public static void main(String[]args) {
+		int vehical = 2;
+		switch(vehical) {
+		case 1:
+			System.out.println("Bike..");
+			break;
+		case 2:
+			System.out.println("Car..");
+			break;
+		case 3:
+			System.out.println("Bus..");
+			break;
+			default:
+				System.out.println("Invalid bikes");
+		}
+	}
+
+}
