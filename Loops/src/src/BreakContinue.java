9fc974140b501d747b55576c6850df41371ab063
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-18 13:49:13 +0530

    Add files via upload
    break and continue statements in for loop

---
+++ Loops/src/src/BreakContinue.java
@@ -0,0 +1,34 @@
+package src;
+
+public class BreakContinue {
+//	public static void main(String[]args) {
+//		for (int i = 1; i <=5; i++) {
+//			if(i==10) {
+//				break;
+//			}
+//			System.out.println(i);		}
+//	}
+	
+	
+	
+//	public static void main(String[]args) {
+//		for (int i=1; i<= 10; i++ ) {
+//			if(i==10) {
+//				break;
+//			}System.out.println(i);
+//		}
+//	}
+	
+	
+	
+	public static void main(String[]args) {
+		for (int i = 2; i<=8; i++) {
+			if (i == 4) {
+				break;
+			}System.out.println(i);
+		}
+	}
+	
+	
+	
+}
