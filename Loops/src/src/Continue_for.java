fb57edef5f898443b1b7285e71879c36441085af
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-18 14:37:12 +0530

    Add files via upload

---
+++ Loops/src/src/Continue_for.java
@@ -0,0 +1,69 @@
+package src;
+
+public class Continue_for {
+//	public static void main(String[]args) {
+//	for (int i = 1; i <= 10; i++) {
+//		if (i == 5) {
+//			continue; // it is used to skip one iteration 
+//		}
+//		System.out.println(i);
+//	  }
+//  }
+//	
+//	
+	
+	
+//	public static void main(String[]args) {
+//		for ( int i = 2; i <= 20; i++) {
+//			if (i == 10) {
+//				continue;
+//			}System.out.println(i);
+//		}
+//		
+//	}
+	
+	
+	
+//	 
+	
+	
+//	public static void main
+//	
+//	(String[]args) {
+//		for (int i = 1; i<=20; i++) {
+//			if(i%3 !=0) {
+//				continue;
+//			}System.out.println(i);
+//		}
+//	}
+//	
+	
+//	public static void main(String[]args) {
+//		for(int i=1; i<=20; i++) {
+//			if(i%4 != 0) {
+//				continue;
+//			}System.out.println(i);
+//		}
+//	}
+	
+//	public static void main(String[]args) {
+//		for (int i = 1; i<=30; i ++) {
+//			if(i%3 == 0) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+//	}
+	
+	
+	public static void main(String[]args) {
+		for (int i = 1; i<= 40; i++) {
+			if (i%4 ==0) {
+				continue;
+			}System.out.println(i);
+		}
+	}
+}
+	
+
+
