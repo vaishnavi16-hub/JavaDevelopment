43e166c94e3f8290531d91fd1c46e8221ba56698
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-20 16:08:42 +0530

    Add files via upload

---
+++ PracticeOuestions/src/CalculateArea.java
@@ -0,0 +1,15 @@
+
+public class CalculateArea {
+	public static void main(String[]args) {
+		int r = 5;
+		System.out.println("circle = "+(3.14*r*r));
+		
+		int s = 6;
+		System.out.println("square = "+(s*s));
+		
+		int l = 5;
+		 int b = 4;
+		System.out.println("Rectangle = " +(l* b));
+	}
+
+}
---
+++ PracticeOuestions/src/StudentData.java
@@ -0,0 +1,21 @@
+public class StudentData {
+    public static void main(String[] args) {
+
+        // create object
+        Student s1 = new Student();
+        
+//        Assign values
+        s1.id = 12;
+        s1.name = "vaishnavi";
+        
+        System.out.println(" Student id = "+ (12));
+        System.out.println("Student name = "+("vaishnavi")); 
+        
+        
+        System.out.println("----------------------");
+        
+        Student s2 = new Student();
+        System.out.println("Student id = "+(14));
+        System.out.println("Student name = "+("saniya")); 
+    }
+}
