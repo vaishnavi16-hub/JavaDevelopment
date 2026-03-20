9702be641e70fe29a91224bada9ee799c277f888
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-20 16:29:23 +0530

    Add files via upload

---
+++ PracticeOuestions/src/BankData.java
@@ -0,0 +1,22 @@
+
+public class BankData {
+private int balance;
+
+public static void main(String[]args) {
+	
+	
+//	create object
+	BankData b1 = new BankData();
+	
+	
+//	Assign values
+   b1.balance = 10000;
+   
+   
+   
+   
+   if(b1.balance>1000) {
+	   System.out.println("Low balanace...!");
+   }
+  }
+}
