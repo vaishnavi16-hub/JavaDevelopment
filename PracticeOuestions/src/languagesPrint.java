773bd6b9dfb7ae76b9a85ed4e42874b7c84a287f
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-20 16:29:56 +0530

    Add files via upload

---
+++ PracticeOuestions/src/languagesPrint.java
@@ -0,0 +1,28 @@
+
+public class languagesPrint { 
+	public static void main(String[]args) {
+		int number = 2;
+		switch(number) {
+		case 1:{
+			System.out.println("Python");
+			break;
+		}
+		case 2:{
+			System.out.println("Java");
+			break;
+		}
+		case 3:{
+			System.out.println("C++");
+			break;
+		}
+		case 4:{
+			System.out.println("Javascript");
+			break;
+		}
+		default:{
+			System.out.println("Invalid ");
+		}
+		}
+	}
+
+}
