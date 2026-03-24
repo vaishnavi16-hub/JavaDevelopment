a07a54820e45ac49ba0b455b82c22b89667a684b
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-24 15:49:04 +0530

    Add files via upload

---
+++ Methods/src/tki/Employee.java
@@ -0,0 +1,22 @@
+package tki;
+
+public class Employee {
+
+	int empId;
+	String name;
+	String city;
+	
+	
+	
+//	printing the variables, attributes and entites....
+	
+public void display() {
+	System.out.println("Employee Information....!");
+	System.out.println("Employee Name = Ram");
+	System.out.println("Employee city = Pune");
+	System.out.println("Employee id = 234");
+     
+
+
+}	
+}
---
+++ Methods/src/tki/MainClass.java
@@ -0,0 +1,19 @@
+package tki;
+
+public class MainClass {
+  public static void main(String[]args) {
+	  
+	  
+	  
+//	  create the object
+	  Employee 	 e = new Employee();
+	  
+	  e.empId = 234;
+	  e.name = "Ram";
+	  e.city = "Pune";
+	  
+//	  calling the method
+	  e.display();
+  }
+
+}
