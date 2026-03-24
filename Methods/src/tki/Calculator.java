2bdcabb60e5debbee57808d36da94d974d53849c
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-24 16:19:36 +0530

    Add files via upload

---
+++ Methods/src/tki/Calculator.java
@@ -0,0 +1,14 @@
+package tki;
+
+public class Calculator {
+	int a;
+	int b;
+
+	
+	public void display() {
+		System.out.println("Addtion  is = "+(a+b));
+		System.out.println("Subtraction is =" +(a-b));
+		System.out.println("Multiplication is =" +(a*b));
+		System.out.println("Division is =" +(a-b));
+	}
+}
---
+++ Methods/src/tki/calculatorData.java
@@ -0,0 +1,20 @@
+package tki;
+
+public class calculatorData {
+public static void main(String[]args) {
+	
+//	creating the objects
+	
+	Calculator c = new Calculator();	
+	
+//	Accessing the 
+
+	c.a = 2;
+	c.b = 5;
+	
+		
+	
+	c.display();
+	
+	}
+}
