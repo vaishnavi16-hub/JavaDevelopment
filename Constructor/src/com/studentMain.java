792d4b42d107e5ce91ea81bbccf5002a2461105f
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-26 16:40:15 +0530

    Add files via upload

---
+++ Constructor/src/com/studentMain.java
@@ -0,0 +1,10 @@
+package com;
+
+public class studentMain {
+    public static void main(String[] args) {
+
+//        Student s = new Student("Ram", 32, "Pune", 9876543210L);
+    	Student s= new Student ("Ram",32,"Pune",2346783627L);
+
+    }
+}
