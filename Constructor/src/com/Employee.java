fe718de3bd65a70ae35c3fc6b1d52a8bcb0454a3
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-27 11:36:35 +0530

    Add files via upload

---
+++ Constructor/src/com/Employee.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Employee {
+ public Employee(String name,int id, String city, int salary, String location) {
+	 System.out.println("Employee inforation ------->");
+	 System.out.println("Employee id = "+ id);
+	 System.out.println("Employe city = "+ city);
+	 System.out.println("Employe salary = "+ salary);
+	 System.out.println("Employe city = "+ location);
+
+	 
+	 
+ }
+}
---
+++ Constructor/src/com/EmployeeMain.java
@@ -0,0 +1,9 @@
+package com;
+
+public class EmployeeMain {
+	public static void main(String[]args) {
+		Employee e = new  Employee("vaishnavi", 1234,"pune",3000030,"mumbai");
+			
+	}
+
+}
