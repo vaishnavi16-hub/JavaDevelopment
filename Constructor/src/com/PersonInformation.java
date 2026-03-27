713f05880aa68c1d4d1bb442e0133546fbe5f0c5
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-27 11:16:37 +0530

    Add files via upload

---
+++ Constructor/src/com/PersonInformation.java
@@ -0,0 +1,16 @@
+package com;
+
+public class PersonInformation {
+	public PersonInformation( String name, int Age, String city ,String Gender, String status) {
+		System.out.println("Person Information ...");
+		System.out.println("Person Name is = "+ name);
+		System.out.println("Person Age is ="+Age);
+		System.out.println("Personn location is ="+ city);
+		System.out.println("Person Gender is ="+ Gender);
+		System.out.println("Person Status is ="+ status);
+
+		
+		
+	}
+
+}
---
+++ Constructor/src/com/PersonMain.java
@@ -0,0 +1,8 @@
+package com;
+
+public class PersonMain {
+
+	public static void main(String[]args) {
+		PersonInformation p = new PersonInformation( "vaishnavi",21, "Pune","Female","unmarried");
+	}
+}
