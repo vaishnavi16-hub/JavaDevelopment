ebf287a94504a41df69c2de980d5a35ffe8b1e6d
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-28 15:00:04 +0530

    Add files via upload

---
+++ Encapsulation/src/Meta.java
@@ -0,0 +1,16 @@
+
+
+public class Meta {
+
+	public static void main(String[] args) {
+
+		Instagram insta = new Instagram();
+		insta.setUserName("vaishu@gmail.com");
+		insta.setPassword("vaishu@123");
+
+		System.out.println(insta.getUserName());
+		System.out.println(insta.getPassword());
+
+	}
+
+}
