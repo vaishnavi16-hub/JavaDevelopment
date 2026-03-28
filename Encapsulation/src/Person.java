436e8ca8aa26b230916b9466404ce4177bddb523
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-03-28 16:01:11 +0530

    Add files via upload

---
+++ Encapsulation/src/Person.java
@@ -0,0 +1,26 @@
+
+public class Person {
+ int personage;
+ String personName;
+ 
+//  create the public get set methods....
+ 
+ public int getpersonage() {
+	 return personage;
+ }
+ 
+ public void setpersonage(int personage) {
+	 this.personage= personage;
+ }
+ 
+
+ public String personName() {
+	 return personName;
+ }
+ 
+ public void setpersonName(String personName) {
+	 this.personName=personName;
+	 
+	 
+ }
+}
---
+++ Encapsulation/src/personMain.java
@@ -0,0 +1,17 @@
+
+public class personMain {
+	public static void main(String[]args) {
+		
+		Person p = new Person();
+		p.setpersonage(23);
+		p.setpersonName("vaishnavi");
+		
+		
+		System.out.println("Person information................>");
+		System.out.println("person age = "+p.getpersonage());
+		System.out.println("person Name = "+p.personName());
+		
+		
+	}
+
+}
