c8a22ea8723dd64043e8075ec58cc5da5d5f5359
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-04-02 00:43:19 +0530

    Add files via upload

---
+++ Inheritance/src/SavingAccount.java
@@ -0,0 +1,7 @@
+
+public class SavingAccount  extends BankAccount{
+	void saveaccount() {
+	System.out.println(" save you account");
+
+	}
+}
---
+++ Inheritance/src/mainClass.java
@@ -0,0 +1,13 @@
+
+public class mainClass {
+   public static void main(String[]args) {
+	   
+	   Product p = new Product();
+	   p.displayproduct();
+	   
+	   System.out.println("---------------------->");
+	   
+	   Electronics e = new Electronics();
+	   e.showwarranty();
+   }
+}
