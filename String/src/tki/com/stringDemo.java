304ab766a90b1b8cfc1b26a1fad83858ae3d9773
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-04-16 20:05:12 +0530

    Add files via upload

---
+++ String/src/tki/com/stringDemo.java
@@ -0,0 +1,14 @@
+package tki.com;
+
+public class stringDemo {
+	public static void main(String[]args) {
+		String str1 = "vaishnavi";
+		String str2 = "gaikwad";
+		String str3 = "vaishnavi";
+	
+	
+       System.out.println(str1.hashCode());
+       System.out.println(str2.hashCode());
+       System.out.println(str3.hashCode());
+	}
+}
