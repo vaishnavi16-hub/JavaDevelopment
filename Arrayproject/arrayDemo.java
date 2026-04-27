c8c1723f56722aaa1960349cf32e9c522ce2d1c4
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-04-28 00:01:31 +0530

    Add files via upload

---
+++ Arrayproject/arrayDemo.java
@@ -0,0 +1,18 @@
+package com.tka;
+
+public class arrayDemo {
+
+	public static void main(String[]args) {
+//		int[] arr = {23,40,60,50};
+		int [] arr = {1,2,3,4,5};
+		
+//		for (int i = 0; i< arr.length; i++) {
+//			System.out.println(arr[i]);
+		
+		
+		
+		for (int i = 0; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
