56809ecdb7ce102a9ef6d7c2c243b8d983387794
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-04-27 23:43:49 +0530

    Add files via upload

---
+++ Arrayproject/Stringarray.java
@@ -0,0 +1,11 @@
+package com.tka;
+
+public class Stringarray {
+	public static void main(String[]args) {
+		String [] name = {"ram","shyam","mohan","shree"};
+		for (int i = 0; i<= name.length; i++) {
+			System.out.println(name[i]);
+		}
+	}
+
+}
