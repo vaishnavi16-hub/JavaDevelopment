c8d04de91f496ffb69a7b2bd1a22fc093e8e3573
vaishnavi gaikwad <vaishnavi200316@gmail.com>
2026-04-28 10:39:05 +0530

    Add files via upload

---
+++ Arrayproject/arrayOddno.java
@@ -0,0 +1,28 @@
+package com.tka;
+
+public class arrayOddno {
+	
+	
+//	odd no
+////	public static void main(String[]args) {
+////		int [] arr = {3,4,5,6,7};
+////		for (int i = 0; i < arr.length;i++) {
+////			if (arr[i] % 2== 1) {
+////				System.out.println(arr[i]);
+////			}
+////		}
+//	}
+
+	
+	
+//	even no
+	
+	public static void main(String[]args) {
+		int [] arr = {3,4,5,6};
+		for (int i = 0; i< arr.length; i++) {
+			if(arr[i] % 3 == 2) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+}
